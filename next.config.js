670cc8628ce31bccdea9351ac170a02aa117fc0c
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-22 15:29:45 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Evitar prerenderizado de páginas que usan autenticación
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
