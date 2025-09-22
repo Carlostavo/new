ab9ca520f362b072170098267a5d5ecd745ba628
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-22 16:41:33 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Deshabilitar prerenderizado para páginas con autenticación
   trailingSlash: true,
   images: {
     unoptimized: true
